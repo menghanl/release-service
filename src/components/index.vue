30625834e788cc0d85c136115aebd6b67715650c
Menghan Li <menghanl@google.com>
2018-04-30 17:39:43 -0700

    add index router

---
+++ src/components/index.vue
@@ -0,0 +1,9 @@
+<template>
+    <b-button to="/release/grpc/grpc-go/v1.12.0">release</b-button>
+</template>
+
+<script>
+export default {
+  name: "index"
+};
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from "vue";
 import Router from "vue-router";
+import index from "@/components/index";
 import release from "@/components/release";
 
 Vue.use(Router);
@@ -9,6 +10,11 @@ const NotFound = { template: "<div>Not Found</div>" };
 export default new Router({
   mode: "history",
   routes: [
+    {
+      path: "/",
+      name: "index",
+      component: index
+    },
     {
       path: "/release/:org/:repo/:version",
       name: "release",
